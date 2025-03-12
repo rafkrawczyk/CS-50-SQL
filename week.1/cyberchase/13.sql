a36fd0a35e98e8ee4dbc46436d44b96066929932
rafkrawczyk <54412881+rafkrawczyk@users.noreply.github.com>
2025-03-12 22:02:59 +0100

    Add files via upload

---
+++ week.1/cyberchase/13.sql
@@ -0,0 +1,3 @@
+SELECT COUNT ("title") FROM "episodes"
+WHERE "air_date" BETWEEN '2020-01-01' AND '2022-06-31'
+OR "season" = 12;
---
+++ week.1/views/8.sql
@@ -0,0 +1,4 @@
+SELECT "english_title" FROM "views"
+WHERE "artist" LIKE 'Hokusai'
+ORDER BY ("contrast") ASC
+LIMIT 5;
